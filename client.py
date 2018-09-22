5fc789712f22d3e82d95985e87e236b553bac6ba
André La Rocca <andrelarocca@gmail.com>
2018-09-21 22:38:41 -0300

    Adding params to clent

--- client.py
+++ client.py
@@ -1,45 +1,21 @@
-#!/usr/bin/env python
-
-###########################################
-#               Redes - TP1               #
-#                                         # 
-# Autores: Joao Francisco B. S. Martins   #
-#          Vitor Bernardo Rodrigues Jorge #                 
-#                                         # 
-# Matriculas: 2013007498                  #
-#             2013007803                  #
-###########################################
-
-# PARA EXECUTAR
-# Socket com abertura passiva: python dcc023c2.py -s <PORT> <INPUT> <OUTPUT>
-# Socket com abertura ativa: python dcc023c2.py -c <IP>:<PORT> <INPUT> <OUTPUT>
-#
-# Para executarmos o script diretamente(assim como em C):
-# chmod +x ./dcc023c2
-# ./dcc023c2 <ARGS>
-
 import sys
 import socket
 import time
 import data_link_utils
 
-MODE = sys.argv[1]  # Passive or active socket
-HOST = (sys.argv[2].split(":"))[0] if (MODE == "-c") else "127.0.0.1" #socket.gethostbyname(socket.getfqdn()) 
-PORT = int((sys.argv[2].split(":"))[1]) if (MODE == "-c") else int(sys.argv[2])
+FILE = sys.argv[1]  # File to read log messages
+HOST = (sys.argv[2].split(":"))[0]
+PORT = int((sys.argv[2].split(":"))[1])
+WTX = int(sys.argv[3]) # Transmission window size
+TOUT = int(sys.argv[4]) # TImeout
+PERROR = int(sys.argv[5]) # Error probability
+
 ADDR = (HOST, PORT)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.connect(ADDR)
+client_socket.settimeout(1) # Socket timeout 1s
 
-dcc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-if MODE == "-c":
-	dcc_socket.connect(ADDR)
-else:
-	dcc_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevents "Address already in use" error
-	dcc_socket.bind(ADDR)
-	dcc_socket.listen(1)
-	dcc_socket = dcc_socket.accept()[0]
-
-input_file  = open(sys.argv[3], "rb")
-output_file = open(sys.argv[4], "wb")
+input_file  = open(FILE, "rb")
 
 max_payload_length = (2 ** 16) - 1  # Greatest number of bytes that the length field can address
 
@@ -53,7 +29,6 @@ last_chksum  = 0     # chksum from last frame
 end_send     = False # If the last frame was already sent, save it to mark the end of data transmission
 end_rcv      = False # If the last frame was already received, save it to mark the end of data reception
 
-dcc_socket.settimeout(1) # Socket timeout 1s
 while True:
 	try:
 		if not is_last_send and not waiting_ack:  # There is still data to send
@@ -63,13 +38,13 @@ while True:
 				if is_last_send: end_send = True 
 				last_send_id = cur_send_id 
 
-			data_link_utils.send_frame(dcc_socket, cur_frame)
+			data_link_utils.send_frame(client_socket, cur_frame)
 			waiting_ack = True 
 
 			timer = time.time()  # Timer that will "wait" for 1 second before retransmission
 
 		# Check buffer for data
-		payload, chksum, frame_id, is_last_rcv, is_ack = data_link_utils.receive_frame(dcc_socket)
+		payload, chksum, frame_id, is_last_rcv, is_ack = data_link_utils.receive_frame(client_socket)
 		if is_last_rcv: end_rcv = True
 
 		if is_ack and frame_id == cur_send_id: # ACK for last sent frame has arrived
@@ -87,11 +62,11 @@ while True:
 
 				# Create and send acknowledge message for the received frame
 				ack = data_link_utils.create_frame(input_file, max_payload_length, last_rcv_id, True)[0]
-				data_link_utils.send_frame(dcc_socket, ack)
+				data_link_utils.send_frame(client_socket, ack)
 			elif frame_id == last_rcv_id and chksum == last_chksum:  # Frame is a retransmission
 				# Resend ack for last received frame
 				ack = data_link_utils.create_frame(input_file, max_payload_length, last_rcv_id, True)[0]
-				data_link_utils.send_frame(dcc_socket, ack)
+				data_link_utils.send_frame(client_socket, ack)
 
 		# End of communication. ACK for last received frame was already sent.
 		if end_rcv and end_send and not waiting_ack:
@@ -106,9 +81,9 @@ while True:
 	except socket.timeout:  # 1 second has been passed in the blocking receive. If is waiting for ack, retransmit last frame.
 		if waiting_ack:
 			print "Timeout. Retransmiting frame\n"
-			data_link_utils.send_frame(dcc_socket, cur_frame)
+			data_link_utils.send_frame(client_socket, cur_frame)
 			timer = time.time()  # Reset timer
 
 input_file.close()
 output_file.close()
-dcc_socket.close()
+client_socket.close()
